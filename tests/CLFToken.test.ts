6458fb06ce45c9906de440d65fd36b8d48f63495
mohit <rayalacheruvu.m@northeastern.edu>
2024-08-25 00:23:54 -0400

    added token tests - transfer b/w accounts and unauth transfer

--- tests/CLFToken.test.ts
+++ tests/CLFToken.test.ts
@@ -52,4 +52,79 @@ describe(`${contractName} contract tests`, () => {
 
     block.receipts[0].result.expectErr().expectUint(100);
   });
+
+  it("should transfer tokens between accounts", () => {
+    let chain = new Chain();
+
+    chain.mineBlock([
+      Tx.contractCall(
+        contractName,
+        "mint",
+        [types.uint(1000), types.principal(recipient1.address)],
+        owner.address
+      ),
+    ]);
+
+    let transferBlock = chain.mineBlock([
+      Tx.contractCall(
+        contractName,
+        "transfer",
+        [
+          types.uint(500),
+          types.principal(recipient1.address),
+          types.principal(recipient2.address),
+          types.none(),
+        ],
+        recipient1.address
+      ),
+    ]);
+
+    transferBlock.receipts[0].result.expectOk().expectBool(true);
+
+    let balance1 = chain.callReadOnlyFn(
+      contractName,
+      "get-balance",
+      [types.principal(recipient1.address)],
+      recipient1.address
+    );
+    balance1.result.expectUint(500);
+
+    let balance2 = chain.callReadOnlyFn(
+      contractName,
+      "get-balance",
+      [types.principal(recipient2.address)],
+      recipient2.address
+    );
+    balance2.result.expectUint(500);
+  });
+
+  it("should not allow transfer from non-owner", () => {
+    let chain = new Chain();
+
+    chain.mineBlock([
+      Tx.contractCall(
+        contractName,
+        "mint",
+        [types.uint(1000), types.principal(recipient1.address)],
+        owner.address
+      ),
+    ]);
+
+    let transferBlock = chain.mineBlock([
+      Tx.contractCall(
+        contractName,
+        "transfer",
+        [
+          types.uint(500),
+          types.principal(recipient2.address),
+          types.principal(recipient1.address),
+          types.none(),
+        ],
+        recipient2.address
+      ),
+    ]);
+
+    transferBlock.receipts[0].result.expectErr().expectUint(101);
+  });
+  
 })
