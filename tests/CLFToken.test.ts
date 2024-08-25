e37a7b0d2d2d9632b326cfcdcefdc000ed89a1c6
mohit <rayalacheruvu.m@northeastern.edu>
2024-08-25 00:33:27 -0400

    added token tests - non-owner transfer &token details correctly

--- tests/CLFToken.test.ts
+++ tests/CLFToken.test.ts
@@ -126,5 +126,100 @@ describe(`${contractName} contract tests`, () => {
 
     transferBlock.receipts[0].result.expectErr().expectUint(101);
   });
-  
-})
+
+  it("should respect the maximum supply", () => {
+    let chain = new Chain();
+
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        contractName,
+        "mint",
+        [types.uint(900000), types.principal(recipient1.address)],
+        owner.address
+      ),
+      Tx.contractCall(
+        contractName,
+        "mint",
+        [types.uint(200000), types.principal(recipient2.address)],
+        owner.address
+      ),
+    ]);
+
+    block.receipts[0].result.expectOk().expectBool(true);
+    block.receipts[1].result.expectErr().expectUint(102);
+  });
+
+  it("should transfer ownership", () => {
+    let chain = new Chain();
+
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        contractName,
+        "set-owner",
+        [types.principal(recipient1.address)],
+        owner.address
+      ),
+    ]);
+
+    block.receipts[0].result.expectOk().expectPrincipal(recipient1.address);
+
+    let newOwner = chain.callReadOnlyFn(
+      contractName,
+      "get-owner",
+      [],
+      recipient1.address
+    );
+    newOwner.result.expectPrincipal(recipient1.address);
+  });
+
+  it("should not allow non-owner to transfer ownership", () => {
+    let chain = new Chain();
+
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        contractName,
+        "set-owner",
+        [types.principal(recipient2.address)],
+        recipient1.address
+      ),
+    ]);
+
+    block.receipts[0].result.expectErr().expectUint(100);
+  });
+
+  it("should return token details correctly", () => {
+    let chain = new Chain();
+
+    let name = chain.callReadOnlyFn(
+      contractName,
+      "get-name",
+      [],
+      owner.address
+    );
+    name.result.expectOk().expectAscii("clarifi-token");
+
+    let symbol = chain.callReadOnlyFn(
+      contractName,
+      "get-symbol",
+      [],
+      owner.address
+    );
+    symbol.result.expectOk().expectAscii("CLF");
+
+    let decimals = chain.callReadOnlyFn(
+      contractName,
+      "get-decimals",
+      [],
+      owner.address
+    );
+    decimals.result.expectOk().expectUint(6);
+
+    let totalSupply = chain.callReadOnlyFn(
+      contractName,
+      "get-total-supply",
+      [],
+      owner.address
+    );
+    totalSupply.result.expectOk().expectUint(0);
+  });
+});
