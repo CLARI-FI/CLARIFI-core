3b66fe1f69286193dc9899bfc01f91247de57735
Mahith Chigurupati <saimahith.chigurupati@gmail.com>
2024-08-29 15:31:50 -0400

    CLF marketplace fee test - setting fee percent

--- tests/CLF-Marketplace-fee.test.ts
+++ tests/CLF-Marketplace-fee.test.ts
@@ -12,4 +12,42 @@ describe(`${contractName} contract tests`, () => {
     owner = accounts.get("deployer")!;
     user = accounts.get("wallet_1")!;
   });
+
+  it("should allow the owner to set a new fee percentage", () => {
+    let chain = new Chain();
+
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        contractName,
+        "set-fee-percentage",
+        [types.uint(10)],
+        owner.address
+      ), // Set fee to 10%
+    ]);
+
+    block.receipts[0].result.expectOk().expectUint(10);
+
+    let feePercentage = chain.callReadOnlyFn(
+      contractName,
+      "get-fee-percentage",
+      [],
+      owner.address
+    );
+    feePercentage.result.expectUint(10);
+  });
+
+  it("should not allow a non-owner to set a new fee percentage", () => {
+    let chain = new Chain();
+
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        contractName,
+        "set-fee-percentage",
+        [types.uint(10)],
+        user.address
+      ),
+    ]);
+
+    block.receipts[0].result.expectErr().expectUint(100);
+  });
 });
